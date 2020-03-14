e6162b1aaf68063f530ff298db4e9a1803d91857
Ghostfyx <1078101544@qq.com>
2020-03-14 18:29:18 +0800

    Flink初始化与wordcount入门

---
+++ src/main/java/com/flink/learning/simple_example/BatchWordCount.java
@@ -0,0 +1,40 @@
+package com.flink.learning.simple_example;
+
+import org.apache.flink.api.common.functions.FlatMapFunction;
+import org.apache.flink.api.java.DataSet;
+import org.apache.flink.api.java.ExecutionEnvironment;
+import org.apache.flink.api.java.tuple.Tuple2;
+import org.apache.flink.util.Collector;
+
+
+/**
+ * @Description: Flink入门程序 批处理wordcount
+ * @Author: FanYueXiang
+ * @Date: 2020/3/14 5:29 PM
+ */
+public class BatchWordCount {
+
+    public static void main(String[] args) throws Exception {
+        // 创建执行环境
+        ExecutionEnvironment env = ExecutionEnvironment.getExecutionEnvironment();
+        // 加载或创建源数据
+        DataSet<String> text = env.readTextFile("/Users/yuexiangfan/coding/JavaProject/flink_learning/src/main/java/com/flink/learning/simple_example/wordcount.txt");
+        // 转化处理数据
+        DataSet<Tuple2<String, Integer>> ds = text.flatMap(new LineSplitter()).groupBy(0).sum(1);
+        // 输出数据到目的端
+        ds.print();
+        // 执行任务操作
+        // 由于是Batch操作，当DataSet调用print方法时，源码内部已经调用Excute方法，所以此处不再调用，如果调用会出现错误
+        //env.execute("Flink Batch Word Count By Java");
+
+    }
+
+    static class LineSplitter implements FlatMapFunction<String, Tuple2<String,Integer>> {
+        @Override
+        public void flatMap(String line, Collector<Tuple2<String, Integer>> collector) throws Exception {
+            for (String word:line.split(",")) {
+                collector.collect(new Tuple2<>(word,1));
+            }
+        }
+    }
+}
---
+++ src/main/java/com/flink/learning/simple_example/StreamingWordCount.java
@@ -0,0 +1,58 @@
+package com.flink.learning.simple_example;
+
+import org.apache.flink.api.common.functions.FlatMapFunction;
+import org.apache.flink.streaming.api.datastream.DataStream;
+import org.apache.flink.streaming.api.datastream.DataStreamSource;
+import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
+import org.apache.flink.util.Collector;
+
+/**
+ * @Description:
+ * @Author: FanYueXiang
+ * @Date: 2020/3/14 6:08 PM
+ */
+public class StreamingWordCount {
+
+    public static void main(String[] args) throws Exception {
+        // 创建执行环境
+        StreamExecutionEnvironment senv = StreamExecutionEnvironment.getExecutionEnvironment();
+        System.out.println("--------------senv-------------");
+        System.out.println(senv);
+        // 设置socket数据源
+        DataStreamSource<String> source = senv.socketTextStream("127.0.0.1",8000, "\n");
+        DataStream<WordWithCount> words = source.flatMap(new FlatMapFunction<String, WordWithCount>() {
+            @Override
+            public void flatMap(String line, Collector<WordWithCount> collector) throws Exception {
+                for (String word : line.split(",")) {
+                    collector.collect(new WordWithCount(word, 1));
+                }
+            }
+        });
+        DataStream<WordWithCount> counts = words.keyBy("word").sum("count");
+        counts.print();
+        // 执行任务操作
+        senv.execute("Flink Streaming Word Count By Java");
+    }
+
+    public static class WordWithCount{
+        public String word;
+        public int count;
+
+        public WordWithCount(){
+
+        }
+
+        public WordWithCount(String word, int count) {
+            this.word = word;
+            this.count = count;
+        }
+
+        @Override
+        public String toString() {
+            return "WordWithCount{" +
+                    "word='" + word + '\'' +
+                    ", count=" + count +
+                    '}';
+        }
+    }
+}
